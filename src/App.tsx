a219ded8d0f0da65a6abde6dc2ebb59776c79bc9
Sandra Hotsyn <sandrahotsy@gmail.com>
2025-12-08 10:29:56 +0200

    add button

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import axios from "axios";
 
 function App() {
   const [character, setCharacter] = useState(null);
+  const [counter, setCounter] = useState(0);
+
   useEffect(() => {
     axios
       .get("http://swapi.info/api/people/5")
@@ -13,6 +15,9 @@ function App() {
     <>
       <h1>Effect</h1>
       <pre>{JSON.stringify(character, null, 2)}</pre>
+      <button onClick={() => setCounter((prevCounter) => prevCounter + 1)}>
+        Count {counter}
+      </button>
     </>
   );
 }
